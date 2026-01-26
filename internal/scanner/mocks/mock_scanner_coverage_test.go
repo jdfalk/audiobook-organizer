0551b91ff62f5a5892c3889bd836f6d567c3e603
Johnathan Falk <johnathan.falk@gmail.com>
2026-01-25 23:15:31 -0500

    feat(tests): add mock coverage tests for various components
    - Added mock_store_coverage_test.go for database.Store interface.
    - Added mock_metadata_extractor_coverage_test.go for metadata.MetadataExtractor interface.
    - Added mock_queue_coverage_test.go for operations.Queue interface.
    - Added mock_scanner_coverage_test.go for scanner.Scanner interface.
    - Removed build constraints from enhanced_test.go, queue_test.go, server_coverage_improvements_test.go, server_coverage_phase2_test.go, and server_queue_test.go.
    
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

---
+++ internal/scanner/mocks/mock_scanner_coverage_test.go
@@ -0,0 +1,62 @@
+package mocks
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jdfalk/audiobook-organizer/internal/scanner"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestMockScanner_Coverage(t *testing.T) {
+	m := NewMockScanner(t)
+	exp := m.EXPECT()
+	ifaceType := reflect.TypeOf((*scanner.Scanner)(nil)).Elem()
+
+	for i := 0; i < ifaceType.NumMethod(); i++ {
+		method := ifaceType.Method(i)
+		name := method.Name
+
+		matcherArgs := make([]reflect.Value, 0, method.Type.NumIn())
+		for j := 0; j < method.Type.NumIn(); j++ {
+			matcherArgs = append(matcherArgs, reflect.ValueOf(mock.Anything))
+		}
+
+		expMethod := reflect.ValueOf(exp).MethodByName(name)
+		if !expMethod.IsValid() {
+			t.Fatalf("EXPECT() missing method %s", name)
+		}
+		callVals := expMethod.Call(matcherArgs)
+		if len(callVals) != 1 {
+			t.Fatalf("EXPECT().%s returned %d values, expected 1", name, len(callVals))
+		}
+		call := callVals[0]
+
+		runMethod := call.MethodByName("Run")
+		if runMethod.IsValid() {
+			runFuncType := runMethod.Type().In(0)
+			runFn := reflect.MakeFunc(runFuncType, func(_ []reflect.Value) []reflect.Value { return nil })
+			runMethod.Call([]reflect.Value{runFn})
+		}
+
+		returnMethod := call.MethodByName("Return")
+		if !returnMethod.IsValid() {
+			t.Fatalf("EXPECT().%s missing Return(...)", name)
+		}
+		returnArgs := make([]reflect.Value, 0, method.Type.NumOut())
+		for j := 0; j < method.Type.NumOut(); j++ {
+			returnArgs = append(returnArgs, reflect.Zero(method.Type.Out(j)))
+		}
+		returnMethod.Call(returnArgs)
+
+		methodVal := reflect.ValueOf(m).MethodByName(name)
+		if !methodVal.IsValid() {
+			t.Fatalf("MockScanner missing method %s", name)
+		}
+		callArgs := make([]reflect.Value, 0, method.Type.NumIn())
+		for j := 0; j < method.Type.NumIn(); j++ {
+			callArgs = append(callArgs, reflect.Zero(method.Type.In(j)))
+		}
+		_ = methodVal.Call(callArgs)
+	}
+}
--- internal/server/server_coverage_phase2_test.go
+++ internal/server/server_coverage_phase2_test.go
@@ -1,5 +1,3 @@
-//go:build mocks
-
 package server
 
 import (
