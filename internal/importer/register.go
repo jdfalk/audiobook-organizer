cbca8f434adcb9d1651989ec6f694a1e1fcc308e
Johnathan Falk <johnathan.falk@gmail.com>
2026-05-12 13:26:12 -0400

    feat(importer): register importpath service in serviceregistry (W1.5)

---
+++ internal/importer/register.go
@@ -0,0 +1,20 @@
+// file: internal/importer/register.go
+// version: 1.0.0
+
+package importer
+
+import (
+	"github.com/jdfalk/audiobook-organizer/internal/database"
+	"github.com/jdfalk/audiobook-organizer/internal/serviceregistry"
+)
+
+func init() {
+	serviceregistry.Register(serviceregistry.ServiceDef{
+		Name:  "importpath",
+		Needs: []string{"store"},
+		Build: func(c *serviceregistry.Container) (any, error) {
+			store := serviceregistry.Get[database.Store](c, "store")
+			return NewImportPathService(store), nil
+		},
+	})
+}
