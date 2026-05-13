20985e311f5c8508e560c7d2fc69ae90d100a9df
Johnathan Falk <johnathan.falk@gmail.com>
2026-05-13 06:18:08 -0400

    feat(merge): register Service in serviceregistry (W2.3)

---
+++ internal/merge/register.go
@@ -0,0 +1,20 @@
+// file: internal/merge/register.go
+// version: 1.0.0
+
+package merge
+
+import (
+	"github.com/jdfalk/audiobook-organizer/internal/database"
+	"github.com/jdfalk/audiobook-organizer/internal/serviceregistry"
+)
+
+func init() {
+	serviceregistry.Register(serviceregistry.ServiceDef{
+		Name:  "merge",
+		Needs: []string{"store"},
+		Build: func(c *serviceregistry.Container) (any, error) {
+			store := serviceregistry.Get[database.Store](c, "store")
+			return NewService(store), nil
+		},
+	})
+}
